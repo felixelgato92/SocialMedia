0006f43019a6bfee1b147b2e624d87e9306427aa
felixelgato92 <ffelixelgato92@gmail.com>
2015-12-09 12:36:54 -0700

    Create SocialMediaEntry.java

---
+++ SocialMediaEntry.java
@@ -0,0 +1,9 @@
+
+/**
+ * Defines a common interface for all social media entries.
+ * @author sburton
+ */
+public interface SocialMediaEntry {
+    public String getUser();
+    public String getPostText();
+}
